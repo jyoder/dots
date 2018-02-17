472f0bb3cd68cf32e4124ec3c2354f365c4455b0
John Yoder <john.yoder@appfolio.com>
2018-02-17 14:42:38 -0800

    Remove unnecessary import from App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import Player from 'state/Player';
 import Dot from 'state/Dot';
 import DotView from 'ui/DotView'
 import DotClickHandler from 'ui/DotClickHandler';
 
 class App extends React.Component {
   render() {
-    const player = new Player('0', 0, 'Gordon');
-    const dot = new Dot.createStandard();
+    const dot = Dot.createStandard();
     const dotClickHandler = new DotClickHandler(
       dot,
       10,
