2ccfe5c59420c35021035a999ab4a53fae4631d2
John Yoder <john.yoder@appfolio.com>
2018-02-17 22:23:24 -0800

    Add initial PlayerToken class

---
+++ src/ui/PlayerToken.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'ui/PlayerToken.css';
+
+export default class PlayerToken extends React.Component {
+    render() {
+        return(
+            <span className='PlayerToken PlayerToken--player1'>A</span>
+        );
+    }
+}
