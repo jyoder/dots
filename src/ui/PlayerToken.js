851b111f10dece5e7760b581ad8b8b498af9572a
John Yoder <john.yoder@appfolio.com>
2018-02-18 14:31:08 -0800

    Parameterize PlayerToken and add tests

--- src/ui/PlayerToken.js
+++ src/ui/PlayerToken.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import 'ui/PlayerToken.css';
 
 export default class PlayerToken extends React.Component {
     render() {
         return(
-            <span className='PlayerToken PlayerToken--player1'>A</span>
+            <span className={this._classes()}>
+                {this._playerInitial()}
+            </span>
         );
     }
+
+    _classes() {
+        return `PlayerToken PlayerToken--player${this.props.player.playerIndex()}`;
+    }
+
+    _playerInitial() {
+        return this.props.player.playerName()[0].toUpperCase();
+    }
 }
+
+PlayerToken.propTypes = {
+    player: PropTypes.object.isRequired
+};
---
+++ src/ui/PlayerToken.test.js
@@ -0,0 +1,26 @@
+import PlayerToken from 'ui/PlayerToken';
+import Player from 'state/Player';
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+test('PlayerToken includes the appropriate class for player 1', () => {
+    const player = new Player('1', 1, 'Marvin');
+    const playerToken = shallow(<PlayerToken player={player}/>);
+    expect(playerToken.hasClass('PlayerToken')).toBeTruthy();
+    expect(playerToken.hasClass('PlayerToken--player1')).toBeTruthy();
+});
+
+test('PlayerToken includes the appropriate class for player 2', () => {
+    const player = new Player('1', 2, 'Marva');
+    const playerToken = shallow(<PlayerToken player={player}/>);
+    expect(playerToken.hasClass('PlayerToken')).toBeTruthy();
+    expect(playerToken.hasClass('PlayerToken--player2')).toBeTruthy();
+});
+
+test('PlayerToken content is the uppercased first letter of the player name', () => {
+    const player = new Player('1', 2, 'marva');
+    const playerToken = shallow(<PlayerToken player={player}/>);
+    expect(playerToken.hasClass('PlayerToken')).toBeTruthy();
+    expect(playerToken.text()).toBe('M');
+});
