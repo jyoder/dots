9711b4a8eb4eeb200420a8d96e4bfe3ff12a04ac
John Yoder <john.yoder@appfolio.com>
2018-02-24 23:50:39 -0800

    Display a board for testing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import React from 'react';
-import Dot from 'state/Dot';
-import Player from 'state/Player';
-import DotView from 'ui/DotView'
-import DotClickHandler from 'ui/DotClickHandler';
-import PlayerToken from 'ui/PlayerToken';
-import PlayerTokenSlug from 'ui/PlayerTokenSlug';
+import Board from 'state/Board';
+import BoardView from 'ui/BoardView';
 
 class App extends React.Component {
   render() {
-    const dot = Dot.createStandard();
-    const dotClickHandler = new DotClickHandler(
-      dot,
-      10,
-      () => { console.log("left") },
-      () => { console.log("top")}
-    );
+    const board = Board.create(10, 10);
 
     return (
       <div className="App">
-        <DotView dot={dot} dotClickHandler={dotClickHandler}>
-          <PlayerToken player={new Player('1', 1, 'George')}/>
-        </DotView>
-        <DotView dot={dot} dotClickHandler={dotClickHandler}>
-          <PlayerToken player={new Player('2', 2, 'Bertha')}/>
-        </DotView>
-        <DotView dot={dot} dotClickHandler={dotClickHandler}>
-          <PlayerTokenSlug/>
-        </DotView>
+        <BoardView board={board} clickHandler={this._handleClick.bind(this)}/>
       </div>
     );
+  }
+
+  _handleClick(lineType, x, y) {
+    console.log(`${lineType}, ${x}, ${y}`);
   }
 }
 
