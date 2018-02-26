70e161fa6bdd02fb5ab9ac44d566c79f6fbc2da4
John Yoder <john.yoder@appfolio.com>
2018-02-25 22:46:02 -0800

    Add ScoreBoardView

---
+++ src/ui/ScoreBoardView.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Table } from 'reactstrap';
+import FontAwesome from 'react-fontawesome';
+
+import PlayerToken from 'ui/PlayerToken';
+
+import 'ui/ScoreBoardView.css';
+
+export default class ScoreBoardView extends React.Component {
+    render() {
+        return(
+            <Table className="ScoreBoardView">
+                <tbody>
+                    {this._rows()}
+                </tbody>
+            </Table>
+        );
+    }
+
+    _rows() {
+        const scores = this.props.game.scores();
+        return Array.from(scores.keys()).map((player) => this._row(player, scores.get(player)));
+    }
+
+    _row(player, score) {
+        return(
+            <tr key={player.playerIndex()} className="ScoreBoardView-row">
+                <td className="ScoreBoardView-row-status">{this._statusIcon(player)}</td>
+                <td className="ScoreBoardView-row-token"><PlayerToken player={player}/></td>
+                <td className="ScoreBoardView-row-name">{player.playerName()}</td>
+                <td className="ScoreBoardView-row-score">{score}</td>
+            </tr>
+        );
+    }
+
+    _statusIcon(player) {
+        return this._activePlayer(player) ? this._activeIcon() : '';
+    }
+
+    _activePlayer(player) {
+        return this.props.game.activePlayer().playerIndex() === player.playerIndex();
+    }
+
+    _activeIcon() {
+        return <FontAwesome name='arrow-right'/>
+    }
+}
+
+ScoreBoardView.propTypes = {
+    game: PropTypes.object.isRequired
+}
---
+++ src/ui/ScoreBoardView.test.js
@@ -0,0 +1,34 @@
+import ScoreBoardView from 'ui/ScoreBoardView';
+import Game from 'state/Game';
+import Player from 'state/Player';
+
+import PlayerToken from 'ui/PlayerToken';
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+test('ScoreBoardView has the proper class', () => {
+    const game = Game.create(3, 3, [new Player('1', 1, 'John')]);
+    const scoreBoardView = shallow(<ScoreBoardView game={game}/>);
+    expect(scoreBoardView.hasClass('ScoreBoardView')).toBeTruthy();
+});
+
+test('ScoreBoardView includes the tokens, names, and scores for all players', () => {
+    const player1 = new Player('1', 1, 'John');
+    const player2 = new Player('2', 2, 'Hillary');
+    const game = Game.create(3, 3, [player1, player2]);
+
+    const scoreBoardView = shallow(<ScoreBoardView game={game}/>);
+    const rows = scoreBoardView.find('.ScoreBoardView-row');
+    expect(rows.length).toBe(2);
+
+    expect(rows.at(0).find(PlayerToken).get(0).props.player).toBe(player1);
+    expect(rows.at(0).find('.ScoreBoardView-row-status').at(0).text().length).toBeGreaterThan(0);
+    expect(rows.at(0).find('.ScoreBoardView-row-name').at(0).text()).toBe('John');
+    expect(rows.at(0).find('.ScoreBoardView-row-score').at(0).text()).toBe('0');
+
+    expect(rows.at(1).find(PlayerToken).get(0).props.player).toBe(player2);
+    expect(rows.at(1).find('.ScoreBoardView-row-status').at(0).text().length).toBe(0);
+    expect(rows.at(1).find('.ScoreBoardView-row-name').at(0).text()).toBe('Hillary');
+    expect(rows.at(1).find('.ScoreBoardView-row-score').at(0).text()).toBe('0');
+});
