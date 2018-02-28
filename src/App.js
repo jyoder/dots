c5bdee043a9e6783e726f2a9e89f869765d812da
John Yoder <john.yoder@appfolio.com>
2018-02-27 21:31:21 -0800

    Increase the game board size

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Player from 'state/Player';
 export default class App extends React.Component {
     render() {
         const players = [new Player('1', 1, 'john'), new Player('2', 2, 'Hillary yoder')];
-        const game = Game.create(4, 4, players);
+        const game = Game.create(8, 8, players);
 
         return (
             <div className="App">
