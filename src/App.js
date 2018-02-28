bc6c1c1b457993fc0dff9fd928f2be4e92f5f829
John Yoder <john.yoder@appfolio.com>
2018-02-27 21:20:18 -0800

    Render the GameView from the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
+import GameView from 'ui/game/GameView';
 import Game from 'state/Game';
 import Player from 'state/Player';
-import BoardView from 'ui/game/BoardView';
-import ScoreBoardView from 'ui/game/ScoreBoardView';
 
-class App extends React.Component {
-  render() {
-    const players = [new Player('1', 1, 'john'), new Player('2', 2, 'Hillary yoder')];
-    const game = Game.create(4, 4, players);
+export default class App extends React.Component {
+    render() {
+        const players = [new Player('1', 1, 'john'), new Player('2', 2, 'Hillary yoder')];
+        const game = Game.create(4, 4, players);
 
-    return (
-      <div className="App">
-        <BoardView game={game} clickHandler={this._handleClick.bind(this)}/>
-        <ScoreBoardView game={game}/>
-      </div>
-    );
-  }
-
-  _handleClick(lineType, x, y) {
-    console.log(`${lineType}, ${x}, ${y}`);
-  }
+        return (
+            <div className="App">
+                <GameView game={game}/>
+            </div>
+        );
+    }
 }
-
-export default App;
