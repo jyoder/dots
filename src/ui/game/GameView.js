a5ae0e6be1997615208d739a63e946d2bf57d1b4
John Yoder <john.yoder@appfolio.com>
2018-03-12 12:03:11 -0700

    Wire the GameView up with the DistributedGame

--- src/ui/game/GameView.js
+++ src/ui/game/GameView.js
@@ -4,21 +4,63 @@ import PropTypes from 'prop-types';
 import BoardView from 'ui/game/BoardView';
 import ScoreBoardView from 'ui/game/ScoreBoardView';
 
+import DistributedGame from 'network/DistributedGame';
+
+import 'ui/game/GameView.css';  
+
 export default class GameView extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            game: null,
+            distributedGame: null
+        };
+    }
+
+    componentWillMount() {
+        this.props.createConnection.then((connection) => {
+            this.setState({ distributedGame: this._distributedGame(connection) });
+        });
+    }
+
     render() {    
-        return (
-            <div className="GameView">
-                <BoardView game={this.props.game} clickHandler={this._handleClick.bind(this)}/>
-                <ScoreBoardView game={this.props.game}/>
-            </div>
+        if(this.state.game) {
+            return(
+                <div className="GameView">
+                    <BoardView game={this.state.game} clickHandler={this._handleClick.bind(this)}/>
+                    <ScoreBoardView game={this.state.game}/>
+                </div>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    _distributedGame(connection) {
+        return DistributedGame.create(
+            connection,
+            this.props.gameId,
+            this._onNextGame.bind(this)
         );
     }
 
     _handleClick(lineType, x, y) {
-        console.log(`${lineType}, ${x}, ${y}`);
+        if(this.state.distributedGame) {
+            if(lineType === 'left') {
+                this.state.distributedGame.markLeftLine(x, y);
+            } else if(lineType === 'top') {
+                this.state.distributedGame.markTopLine(x, y);
+            }
+        }
+    }
+
+    _onNextGame(game) {
+        this.setState({ game: game });
     }
 }
 
 GameView.propsTypes = {
-    game: PropTypes.object.isRequired
+    gameId: PropTypes.string.isRequired,
+    createConnection: PropTypes.object.isRequired
 };
--- src/ui/game/GameView.test.js
+++ src/ui/game/GameView.test.js
@@ -1,11 +1,101 @@
 import GameView from 'ui/game/GameView';
+import BoardView from 'ui/game/BoardView';
+import ScoreBoardView from 'ui/game/ScoreBoardView';
+
 import Game from 'state/Game';
 
 import React from 'react';
 import { shallow } from 'enzyme';
 
-test('GameView renders', () => {
-    const game = Game.create(4, 4);
-    const gameView = shallow(<GameView game={game}/>);
-    expect(gameView.hasClass('GameView')).toBeTruthy();
+describe('GameView', () => {
+    it('renders nothing until it receives the first game update', () => {
+        const createConnection = _createConnectionPromise();
+        const gameView = shallow(<GameView gameId={'gameId'} createConnection={createConnection}/>);
+        expect(gameView.hasClass('GameView')).toBeFalsy();
+
+        const connection = _connection();
+        createConnection.then.mock.calls[0][0](connection);
+        expect(connection.listen.mock.calls[0][0]).toBe('gameId');
+
+        connection.listen.mock.calls[0][1]([
+            {
+                eventType: 'addPlayer',
+                playerId: 'id1',
+                playerName: 'John'
+            }
+        ]);
+        gameView.update();
+        expect(gameView.hasClass('GameView')).toBeTruthy();
+    });
+
+    it('renders a BoardView and a ScoreBoardView with required properties', () => {
+        const createConnection = _createConnectionPromise();
+        const gameView = shallow(<GameView gameId={'gameId'} createConnection={createConnection}/>);
+
+        const connection = _connection();
+        createConnection.then.mock.calls[0][0](connection);
+        connection.listen.mock.calls[0][1]([
+            {
+                eventType: 'addPlayer',
+                playerId: 'id1',
+                playerName: 'John'
+            }
+        ]);
+        gameView.update();
+
+        const boardView = gameView.find(BoardView).get(0);
+        expect(boardView.props.game).toBeInstanceOf(Game);
+        expect(boardView.props.clickHandler).toBeInstanceOf(Function);
+
+        const scoreBoardView = gameView.find(ScoreBoardView).get(0);
+        expect(scoreBoardView.props.game).toBeInstanceOf(Game);
+    });
+
+    it('responds to the user clicking a line by sending an update on the connection', () => {
+        const createConnection = _createConnectionPromise();
+        const gameView = shallow(<GameView gameId={'gameId'} createConnection={createConnection}/>);
+
+        const connection = _connection();
+        createConnection.then.mock.calls[0][0](connection);
+        connection.listen.mock.calls[0][1]([
+            {
+                eventType: 'addPlayer',
+                playerId: 'id1',
+                playerName: 'John'
+            },
+            {
+                eventType: 'addPlayer',
+                playerId: 'id2',
+                playerName: 'Hillary'
+            },
+            {
+                eventType: 'startGame'
+            }
+        ]);
+        gameView.update();
+
+        const boardView = gameView.find(BoardView).get(0);
+        boardView.props.clickHandler('left', 0, 0);
+        expect(connection.addEvent).toHaveBeenCalledWith(
+            'gameId',
+            {
+                eventType: 'markLeftLine',
+                x: 0,
+                y: 0
+            }
+        );
+    });
 });
+
+function _createConnectionPromise() {
+    return {
+        then: jest.fn()
+    }
+}
+
+function _connection() {
+    return {
+        listen: jest.fn(),
+        addEvent: jest.fn()
+    }
+}
